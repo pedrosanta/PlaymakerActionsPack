fa057d733ff994ad147eff7d40c6d30ac76e597f
Pedro Machado Santa <pedro.santa@gmail.com>
2013-01-24 18:30:25 +0000

    Adds Get Signed Angle To Target action

---
+++ GetSignedAngleToTarget.cs
@@ -0,0 +1,85 @@
+/ * Get Signed Angle to Target - Playmaker Custom Action
+  * https://github.com/pedrosanta/PlaymakerActions */
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Transform)]
+	[Tooltip("Gets the signed Angle (in degrees, clockwise, -180 to 180) between a Game Object's forward axis and a Target. The Target can be defined as a Game Object or a world Position. If you specify both, then the Position will be used as a local offset from the Object's position.")]
+	public class GetSignedAngleToTarget : FsmStateAction
+	{
+		[RequiredField]
+		public FsmOwnerDefault gameObject;
+		
+		public FsmGameObject targetObject;
+		
+		public FsmVector3 targetPosition;
+		
+		public FsmBool ignoreHeight;
+		
+		[RequiredField]
+		[UIHint(UIHint.Variable)]
+		public FsmFloat storeAngle;
+		
+		public bool everyFrame;
+		
+		public override void Reset()
+		{
+			gameObject = null;
+			targetObject = null;
+			targetPosition = new FsmVector3 { UseVariable = true};
+			ignoreHeight = true;
+			storeAngle = null;
+			everyFrame = false;
+		}
+
+		public override void OnLateUpdate()
+		{
+			DoGetAngleToTarget();
+			
+			if (!everyFrame)
+			{
+				Finish();
+			}
+		}
+
+		void DoGetAngleToTarget()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go == null)
+			{
+				return;
+			}
+			
+			var goTarget = targetObject.Value;
+			if (goTarget == null && targetPosition.IsNone)
+			{
+				return;
+			}
+
+			Vector3 targetPos;
+			if (goTarget != null)
+			{
+				targetPos = !targetPosition.IsNone ? 
+					goTarget.transform.TransformPoint(targetPosition.Value) : 
+					goTarget.transform.position;
+			}
+			else
+			{
+				targetPos = targetPosition.Value;
+			}
+
+			if (ignoreHeight.Value)
+			{
+				targetPos.y = go.transform.position.y;
+			}
+			
+			var localTarget = go.transform.InverseTransformPoint(targetPos);
+			
+			// This will only work with vectors on XZ plane, so ignore height is irrelevant. Update description/etc asap.
+			storeAngle.Value = Mathf.Atan2(localTarget.x, localTarget.z) * Mathf.Rad2Deg;
+		}
+
+	}
+}
--- README.md
+++ README.md
@@ -8,3 +8,6 @@ Check out the list of available actions below.
 
 ### Animation
 * **Is Animation Playing** - Checks if the specified animation is playing and stores the result in a variable. Checkout the Animation.IsPlaying on the Unity Documentation for further details.
+
+### Transform
+* **Get Signed Angle To Target** - Gets the signed Angle (in degrees, clockwise, -180 to 180) between a Game Object's forward axis and a Target. The Target can be defined as a Game Object or a world Position. If you specify both, then the Position will be used as a local offset from the Object's position.
